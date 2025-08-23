e1b672b05b78b95c3ac65c0842d9d4d79a43ad34
Sebastian Widelak <sebastian.widelak@justpremium.com>
2025-08-23 08:44:53 +0200

    Exclude services directory from git tracking
    - Added services/ to .gitignore
    - Removed services directory from git tracking while keeping files locally

--- services/postgres-db/init/01-init.sql
+++
@@ -1,7 +0,0 @@
--- database service: postgres-db
--- Database: postgres-db
-
-CREATE DATABASE IF NOT EXISTS postgres-db;
-CREATE USER IF NOT EXISTS 'postgres-db'@'%' IDENTIFIED BY 'hdqsj6SxuYRBUZXwdXySKBmEvXElytMZwMJgyVxS0j0=';
-GRANT ALL PRIVILEGES ON postgres-db.* TO 'postgres-db'@'%';
-FLUSH PRIVILEGES;
